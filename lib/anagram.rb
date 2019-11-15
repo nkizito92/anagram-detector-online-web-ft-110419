715be1bc3d1c1f02096f7384aaec1ce5335b5609
Kizito Njoku <nkizito57@gmail.com>
2019-11-15 16:01:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+   attr_accessor :word
+  @@words = []
+    
+    def initialize(word)
+      @word = word
+    end
+    
+  
+  def match(arr_words) 
+    arr_words.select {|w| @word.split("").sort == w.split("").sort}
+  end 
+end 
+
+# anagram = Anagram.new("Hello")
+# anagram.detector
+# anagram
