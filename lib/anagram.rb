54b644cc2d4116fd3c88bb23c7fe93694ab66add
Kizito Njoku <nkizito57@gmail.com>
2019-11-15 15:14:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,30 +8,3 @@ class Anagram
   
   
 end 
-
-
-
-funciton ana(strA, strB){
-  const amap = bul(strA);
-  const bmap = bul(strB);
-  
-  
-  if(Object.keys(amap).length !== Object.keys(bmap).length) {
-    return false 
-  }
-  
-  for(let char in amap){
-    if(amap[char] !== bmap[char]){
-      return false 
-    }
-  }
-  return true
-}
-
-funtion bul(str){
-  const charMap = {}
-  for(let char of str.replace(/[^\w]/g,"").toLowerCase()){
-    charMap[char] = charMap[char] + 1 || 1
-  }
-  return charMap
-}
